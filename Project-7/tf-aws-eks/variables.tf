48fa7c3040a939db6b1ef1fb1a5ab4be4fe2bbd9
Vishal Mishra <vishalmishra@live.in>
2024-03-09 15:53:18 +0800

    Creating EKS Cluster with terraform and CICD

---
+++ Project-7/tf-aws-eks/variables.tf
@@ -0,0 +1,44 @@
+variable "aws_region" {
+    description = "The region where the infrastructure should be deployed to"
+    type = string
+}
+
+variable "aws_account_id" {
+    description = "AWS Account ID"
+    type = string
+}
+
+variable "backend_jenkins_bucket" {
+    description = "S3 bucket where jenkins terraform state file will be stored"
+    type = string
+}
+
+variable "backend_jenkins_bucket_key" {
+    description = "bucket key for the jenkins terraform state file"
+    type = string
+}
+
+variable "vpc_name" {
+  description = "VPC Name for Jenkins Server VPC"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR for Jenkins Server VPC"
+  type        = string
+}
+
+variable "public_subnets" {
+  description = "Subnets CIDR range"
+  type        = list(string)
+}
+
+variable "private_subnets" {
+  description = "Subnets CIDR range"
+  type        = list(string)
+}
+
+variable "instance_type" {
+  description = "Instance Type"
+  type        = string
+}
